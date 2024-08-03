f50946482c78aa539b9841eee0a9df4e65a1421c
Kennguyen230 <kennguyen230@gmail.com>
2024-08-03 01:37:19 -0400

    Fixed coupon card photos

--- src/components/CouponCard.tsx
+++ src/components/CouponCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { AttentionSeeker } from "react-awesome-reveal";
+import pinkflower from "../assets/animals/pinkflower.png";
+import whiteCat from "../assets/animals/white.png";
 
 const CouponCard = ({ showCoupon }) => {
   return (
@@ -8,17 +10,17 @@ const CouponCard = ({ showCoupon }) => {
         <AttentionSeeker effect="tada">
           <div className="h-72 w-96 md:h-[30rem] md:w-[40rem] flex flex-col justify-center items-center gap-5 bg-[#FFE5EC] mb-32 rounded-xl font-amatic relative border-white border-2">
             <img
-              src="src\assets\animals\pinkflower.png"
+              src={pinkflower}
               alt="sakura"
               className="absolute -top-8 -left-10 w-28"
             />
             <img
-              src="src\assets\animals\pinkflower.png"
+              src={pinkflower}
               alt="sakura"
               className="absolute -bottom-8 -right-8 w-24"
             />
             <img
-              src="src\assets\animals\white.png"
+              src={whiteCat}
               alt="sakura"
               className="absolute -bottom-6 -left-4 w-24"
             />
